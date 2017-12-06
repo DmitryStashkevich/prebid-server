fa26528326c5640e44a3f7d5c66e7ab6bb832bf6
Sindhura Katta <sindhurahere@gmail.com>
2017-12-06 13:58:29 -0800

    Logging interface added

--- logging/transactional_logging.go
+++ logging/transactional_logging.go
@@ -1,1 +1,66 @@
 package logging
+
+import (
+	"github.com/rcrowley/go-metrics"
+	"sync"
+)
+
+type DomainMetrics struct {
+	RequestMeter metrics.Meter
+}
+
+type AccountMetrics struct {
+	RequestMeter      metrics.Meter
+	BidsReceivedMeter metrics.Meter
+	PriceHistogram    metrics.Histogram
+	// store account by adapter metrics. Type is map[PBSBidder.BidderCode]
+	AdapterMetrics map[string]*AdapterMetrics
+}
+
+type AdapterMetrics struct {
+	NoCookieMeter     metrics.Meter
+	ErrorMeter        metrics.Meter
+	NoBidMeter        metrics.Meter
+	TimeoutMeter      metrics.Meter
+	RequestMeter      metrics.Meter
+	RequestTimer      metrics.Timer
+	PriceHistogram    metrics.Histogram
+	BidsReceivedMeter metrics.Meter
+}
+
+var (
+	metricsRegistry      metrics.Registry
+	mRequestMeter        metrics.Meter
+	mAppRequestMeter     metrics.Meter
+	mNoCookieMeter       metrics.Meter
+	mSafariRequestMeter  metrics.Meter
+	mSafariNoCookieMeter metrics.Meter
+	mErrorMeter          metrics.Meter
+	mInvalidMeter        metrics.Meter
+	mRequestTimer        metrics.Timer
+	mCookieSyncMeter     metrics.Meter
+
+	adapterMetrics map[string]*AdapterMetrics
+
+	accountMetrics        map[string]*AccountMetrics // FIXME -- this seems like an unbounded queue
+	accountMetricsRWMutex sync.RWMutex
+)
+
+type TransactionLogger interface {
+	logRequest()
+	logResponse()
+}
+
+type GraphiteLogger struct {
+}
+
+func (gl *GraphiteLogger) logRequest() {}
+
+func (gl *GraphiteLogger) logResponse() {}
+
+type FileLogger struct {
+}
+
+func (fl *FileLogger) logRequest() {}
+
+func (fl *FileLogger) logResponse() {}
